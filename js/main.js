d25898e8c09e22ea5de9e13544bab9577b15c74f
pabluchokk <pabloagusrivero17@gmail.com>
2023-09-07 15:55:45 -0300

    Conexión con la PokeAPI e instalación de Sweet Alert

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 class Usuario {
-    constructor (nombre, contraseña) {
-        this.nombre = nombre
-        this.contraseña = contraseña
+    constructor(nombre, contraseña) {
+    this.nombre = nombre;
+    this.contraseña = contraseña;
     }
 }
 
-const logeado = JSON.parse(localStorage.getItem("logeado"))
+const logeado = JSON.parse(localStorage.getItem("logeado"));
 
-logeado ? mostrarPokemones() : formSesion()
+logeado ? mostrarPokemones() : formSesion();
 
 function formSesion() {
-    let usuario = document.getElementById("usuario")
+    let usuario = document.getElementById("usuario");
     usuario.innerHTML = `
     <h2>Iniciar Sesión</h2>
     <form id="login">
@@ -25,12 +25,38 @@ function formSesion() {
     `;
 }
 
-let loginForm = document.getElementById("login")
-loginForm.addEventListener("submit", function(e) {
+let loginForm = document.getElementById("login");
+loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let nombre = document.getElementById("nombre").value;
+    let contraseña = document.getElementById("contraseña").value;
+    const usuario = new Usuario(nombre, contraseña);
+    localStorage.setItem("logeado", JSON.stringify(usuario));
+    location.reload;
+});
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+
+let buscador = document.getElementById("buscador");
+buscador.innerHTML = `
+<input type="text"  placeholder="Busca a tu pokemón..." id="nombrePokemon">
+
+<button id="buscar">Atrapalo!</button>
+`;
+
+function buscarPokemon(nombre) {
+    fetch(`${url}${nombre}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Datos de Pokemon:", data);
+    })
+    .catch((error) => console.error("Ocurrio un error al buscar en la API", error))
+}
+
+let buscar = document.getElementById("buscar")
+
+buscar.addEventListener("click", (e) => {
     e.preventDefault()
-    let nombre = document.getElementById("nombre").value
-    let contraseña = document.getElementById("contraseña").value
-    const usuario = new Usuario(nombre, contraseña)
-    localStorage.setItem("logeado", JSON.stringify(usuario))
-    location.reload
+    let nombrePokemon = document.getElementById("nombrePokemon").value.toLowerCase()
+    buscarPokemon(nombrePokemon);
 })
