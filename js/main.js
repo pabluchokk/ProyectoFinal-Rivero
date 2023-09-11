9c22c75f12bd0e9e2f4c8e70c99633a6ae4b1092
pabluchokk <pabloagusrivero17@gmail.com>
2023-09-11 18:10:47 -0300

    Revisiones finales y algunas correcciones

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-// CONSTRUCTOR PARA EL USUARIO
-
+// CONSTRUCTOR DEL USUARIO
 class Usuario {
     constructor(nombre, contraseña) {
         this.nombre = nombre;
         this.contraseña = contraseña;
     }
 }
+// VERIFICADOR SI EL ITEM "LOGEADO" ESTA EN EL STORAGE
 
 const logeado = JSON.parse(localStorage.getItem("logeado"));
 
 logeado ? mostrarBuscador() : formSesion();
 
+// FORMULARIO DE INICIO DE SESIÓN
 
 function formSesion() {
     let usuario = document.getElementById("usuario");
     usuario.innerHTML = `
     <div id="login">
-        <h2>Iniciar Sesión</h2>
+        <h2>Inicia Sesión y comienza a atrapar algunos Pokemon!</h2>
         <form id="login">
             <label for="nombre">Nombre de Usuario:</label>
             <input type="text" id="nombre" required>
@@ -28,37 +29,38 @@ function formSesion() {
         </form>
     </div>
     `;
+            
+            let loginForm = document.getElementById("login");
+            loginForm.addEventListener("submit", function (e) {
+                e.preventDefault();
+                let nombre = document.getElementById("nombre").value;
+                let contraseña = document.getElementById("contraseña").value;
+                const usuario = new Usuario(nombre, contraseña);
+                localStorage.setItem("logeado", JSON.stringify(usuario));
+                location.reload();
+            });
+        }
 
-let loginForm = document.getElementById("login");
-loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let nombre = document.getElementById("nombre").value;
-    let contraseña = document.getElementById("contraseña").value;
-    const usuario = new Usuario(nombre, contraseña);
-    localStorage.setItem("logeado", JSON.stringify(usuario));
-    location.reload();
-});
-}
+    // FUNCION PARA MOSTRAR EL BUSCADOR DE POKEMON
 
-function mostrarBuscador() {
-    let usuario = document.getElementById("usuario")
-    usuario.innerHTML = `
-    <div id="icon" class="cerrar">Bienvenido Entrenador ${logeado.nombre} | 
-    <button class="logout" id="icon" onclick="cerrarSesion()">Cerrar sesión <i id="icon" class="fa-sharp fa-solid fa-right-from-bracket"></i></button>
-    </div>
-    `;
-
+    function mostrarBuscador() {
+        let usuario = document.getElementById("usuario") // SALUDO Y BOTON DE CIERRE DE SESION
+        usuario.innerHTML = `
+            <div id="icon" class="cerrar">Bienvenido Entrenador ${logeado.nombre} | 
+                <button class="logout" id="icon" onclick="cerrarSesion()">Cerrar sesión <i id="icon" class="fa-sharp fa-solid fa-right-from-bracket"></i></button>
+            </div>
+            `;
+    
     let icon = document.getElementById("icon")
     icon.style.position = "absolute";
     icon.style.top = "10px"
     icon.style.right = "10px"
-    // icon.style.cursor = "pointer"
-
-    let buscador = document.getElementById("buscador")
+    
+    let buscador = document.getElementById("buscador") // BUSCADOR DE POKEMON
     buscador.innerHTML = ` 
-    <input type="text" placeholder="Busca a tu pokemón..." id="nombrePokemon">
-    <button id="buscar">Atrapalo!</button>
-    `;
+        <input type="text" placeholder="Busca a tu pokemón..." id="nombrePokemon">
+        <button id="buscar">Atrapalo!</button>
+        `;
     let buscar = document.getElementById("buscar")
     buscar.addEventListener("click", (e) => {
         e.preventDefault()
@@ -67,10 +69,15 @@ function mostrarBuscador() {
     })
     buscador.style.display = "block"
 }
-function cerrarSesion() {
+
+// CIERRE DE SESION
+
+function cerrarSesion() { 
     localStorage.removeItem("logeado")
     location.reload()
 }
+
+// API, EN ESTE CASO LA "PokeAPI"
 
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -80,48 +87,90 @@ buscador.innerHTML = `
 <button id="buscar">Atrapalo!</button>
 `;
 
-function buscarDatos(data) {
-    let mayuscula = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-
-    let altura = data.height / 10;
-    let peso = data.weight / 10;
-
-    let mensaje = `<img src="${data.sprites.front_default}" alt="Imagen del Pokémon" style="width: 150px; height: auto; display: block; margin: 0 auto;"><br>`;
-    mensaje += `Nombre: ${mayuscula} <br>`;
-    mensaje += `N° de la Pokedex: ${data.id} <br>`;
-    mensaje += `Altura: ${altura} m <br>`;
-    mensaje += `Peso: ${peso} kg <br>`;
-
-    Swal.fire({
-    title: "Pokemon atrapado!",
-    html: mensaje,
-    icon: "success",
-    });
-}
+// CONEXION A LA API PARA BUSCAR POKEMON 
 
 function buscarPokemon(nombre) {
-    fetch(`${url}${nombre}`)
+    fetch(`${url}${nombre}`) 
     .then((response) => response.json())
     .then((data) => {
         buscarDatos(data);
     })
     .catch((error) => {
         Swal.fire({
-        title: "Error!",
-        text: "Al parecer ocurrió un error al buscar en la Pokedex",
-        icon: "error",
-        footer: "Al parecer este pokemon aún no fue descubierto...",
+            title: "Error!",
+            text: "Al parecer ocurrió un error al buscar en la Pokedex",
+            icon: "error",
+            footer: "Al parecer este pokemon aún no fue descubierto...",
         });
         console.error("Ocurrió un error al buscar en la API:", error);
+    });
+}
+
+// ALERTA DE DATOS CON SWEET ALERT 
+
+function buscarDatos(data) {
+    let mayuscula = data.name.charAt(0).toUpperCase() + data.name.slice(1); // PARA QUE EL NOMBRE EMPIECE CON MAYUSCULA
+
+    let altura = data.height / 10; // PARA TRANSFORMAR LA ALTURA A METROS
+    let peso = data.weight / 10; // PARA TRANSFORMAR EL PESO EN KILOS
+
+    let mensaje = `<img src="${data.sprites.front_default}" alt="Imagen del Pokémon" style="width: 150px; height: auto; display: block; margin: 0 auto;"><br>`; // PARA MOSTRAR EN EL MENSAJE EL SPRITE (IMAGEN) DEL POKEMON BUSCADO
+    mensaje += `Nombre: ${mayuscula} <br>`;
+    mensaje += `N° de la Pokedex: ${data.id} <br>`;
+    mensaje += `Altura: ${altura} m <br>`;
+    mensaje += `Peso: ${peso} kg <br>`;
+    mensaje += `<button id="agregar" onclick="agregarEquipo('${mayuscula}')">Añadir al Equipo</button>`;  // BOTON PARA AGREGAR POKEMON AL EQUIPO
+
+    // MENSAJE CON SWEET ALERT
+    Swal.fire({
+    title: "Pokemon atrapado!",
+    html: mensaje,
+    icon: "success",
 });
 }
-let buscar = document.getElementById("buscar");
 
+// EVENTO PARA EJECUTAR LA FUNCION PARA BUSCAR AL POKEMON
+let buscar = document.getElementById("buscar");
 buscar.addEventListener("click", (e) => {
     e.preventDefault();
-    let nombrePokemon = document
-    .getElementById("nombrePokemon")
-    .value.toLowerCase();
+    let nombrePokemon = document.getElementById("nombrePokemon").value.toLowerCase();
     buscarPokemon(nombrePokemon);
 });
 
+
+let equipoPokemon = []
+
+function agregarEquipo(nombrePokemon) {
+    if (equipoPokemon.length < 6) {
+    equipoPokemon.push(nombrePokemon)
+    actualizar()
+
+    Swal.fire({
+            title: "Éxito",
+            text: `${nombrePokemon} ha sido añadido a tu equipo Pokémon.`,
+            icon: "success"
+    })
+    } else { 
+        Swal.fire({
+            title: "Equipo Pokémon Lleno",
+            text: "Tu equipo Pokémon ya tiene 6 Pokémon. Ya puedes liberar algunos combates!",
+            icon: "warning"
+        })
+    }
+}
+
+function actualizar () {
+    let contador = document.getElementById("contador")
+    contador.textContent = equipoPokemon.length
+
+    let lista = document.getElementById("lista")
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild)
+    }
+
+    equipoPokemon.forEach((nombrePokemon) => {
+        let list = document.createElement("li")
+        list.textContent = nombrePokemon
+        lista.appendChild(list)
+    })
+}
